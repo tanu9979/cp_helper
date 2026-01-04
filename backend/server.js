ce213a434fe0e5a4edda28e14b8c6ca538c274fd
Tanima <tanu9979@gmail.com>
2026-01-04 21:46:53 +0530

    Add credentials support to CORS

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ app.use((req, res, next) => {
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
         res.header('Access-Control-Allow-Origin', origin);
+        res.header('Access-Control-Allow-Credentials', 'true');
     }
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Cookie');
