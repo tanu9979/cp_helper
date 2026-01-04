234f5ccef7d5b8f9a3a6e57613465ea8383c3ebb
Tanima samanta <tanimasamanta97@gmail.com>
2026-01-04 21:22:20 +0530

    Add 'Cookie' to allowed CORS headers

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', origin);
     }
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Cookie');
     res.header('Access-Control-Allow-Credentials', 'true');
     if (req.method === 'OPTIONS') {
         res.sendStatus(200);
