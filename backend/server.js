f02e7da50df6e8d567df7c397009ccc190e0e422
Tanima <tanu9979@gmail.com>
2026-01-04 20:51:31 +0530

    Set CORS to allow only Vercel frontend URL

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 
 // CORS
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://cp-helper-rust.vercel.app');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     
